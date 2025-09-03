951de07e843b19609e91ac2c9d02c6edc4d26569
diegoaranab <77321344+diegoaranab@users.noreply.github.com>
2025-09-03 04:49:31 -0600

    feat: add reusable hero video component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HeroVideoComponent } from '../../shared/hero-video/hero-video.component';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, HeroVideoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
