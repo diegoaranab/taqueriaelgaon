3fcece9731a1fae78095c166283e123fdc052a9a
diegoaranab <77321344+diegoaranab@users.noreply.github.com>
2025-09-03 19:51:59 -0600

    Enable active navbar links and lazy-loaded map

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { ConsentBannerComponent } from './components/consent-banner/consent-banner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ConsentBannerComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ConsentBannerComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
